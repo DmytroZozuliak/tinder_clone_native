2bab40bfd3114defdcbd9f7f1201cb756cb3a894
DmytroZozuliak <dmytro.zozuliak@ellorem.com>
2023-02-25 23:22:23 +0200

    feat: no cards view after all swipings

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -15,6 +15,7 @@ const HomeScreen = () => {
   const { user, logOut } = useAuth()
   const swipeRef = useRef<Swiper<UserCard> | null>(null)
   const [profiles, setProfiles] = useState<UserCard[]>([])
+  const [isSwipedAll, setIsSwipedAll] = useState(false)
 
   if (!user) {
     return (
@@ -54,7 +55,12 @@ const HomeScreen = () => {
     return unsubscribe
   }, [])
 
-  // console.log('profiles.length', profiles.length)
+  const swipeLeft = async (cardInd: number) => {
+
+  }
+  const swipeRight = async (cardInd: number) => {
+
+  }
 
   return (
     <SafeAreaView
@@ -63,69 +69,90 @@ const HomeScreen = () => {
 
       {/* Cards */}
       <View className='flex-1'>
-        <Swiper
-          ref={swipeRef}
-          containerStyle={{ backgroundColor: "transparent" }}
-          cards={profiles}
-          stackSize={5}
-          cardIndex={0}
-          animateCardOpacity
-          verticalSwipe={false}
-          infinite={true}
-          overlayLabels={{
-            left: {
-              title: "NOPE",
-              style: {
-                label: {
-                  textAlign: 'right',
-                  color: "red",
-                }
-              }
-            },
-            right: {
-              title: "MATCH",
-              style: {
-                label: {
-                  color: "#4DED30",
-                }
-              }
-            },
-          }}
-          onSwipedLeft={(ind) => {
-            // console.log("index", ind)
-          }}
-          onSwipedRight={() => {
+        {!isSwipedAll ? (
+          <Swiper
+            ref={swipeRef}
+            key={maleCardsData.length}
+            containerStyle={{ backgroundColor: "transparent" }}
+            onSwipedAll={() => {
+              setIsSwipedAll(true)
+            }}
+            // cards={[{
+            //   id: '2',
+            //   displayName: 'Elon',
+            //   job: 'CEO',
+            //   photoURL:
+            //     'https://scontent.fiev7-3.fna.fbcdn.net/v/t39.30808-6/318771351_103632185931933_5455273492949628163_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=cUsu05nYm4cAX9n-oS1&_nc_ht=scontent.fiev7-3.fna&oh=00_AfBOiGSSHNOoDw0k3_6FjuyFVSTHSvZGAuDkCLKJWCuHfg&oe=63FF9CD7',
+            //   age: '43',
+            //   timestamp: new Date(),
+            // }]}
+            cards={maleCardsData}
+            // cards={profiles}
+            // stackSize={5}
+            // cardIndex={0}
+            animateCardOpacity
+            verticalSwipe={false}
+            // infinite={true}
 
-          }}
-          renderCard={(card, ind) => (card) ? (
-            <View key={card.id} className='bg-white h-3/4 rounded-xl relative'>
-              <Image
-                className='h-full w-full rounded-xl'
-                source={{ uri: card.photoURL }} />
-              <View className='flex-row justify-between items-center bg-white  w-full h-20 absolute bottom-0 px-6 py-2 rounded-b-xl' style={styles.cardShadow}>
-                <View>
-                  <Text className='text-xl font-bold'>{card.displayName}</Text>
-                  <Text>{card.job}</Text>
+            overlayLabels={{
+              left: {
+                title: "NOPE",
+                style: {
+                  label: {
+                    textAlign: 'right',
+                    color: "red",
+                  }
+                }
+              },
+              right: {
+                title: "MATCH",
+                style: {
+                  label: {
+                    color: "#4DED30",
+                  }
+                }
+              },
+            }}
+            onSwipedLeft={(cardInd) => {
+              swipeLeft(cardInd)
+              // console.log("index", ind)
+            }}
+            onSwipedRight={(cardInd) => {
+              swipeRight(cardInd)
+            }}
+            renderCard={(card, ind) => {
+
+              return (
+                <View key={card.id} className='bg-white h-3/4 rounded-xl relative'>
+                  <Image
+                    className='h-full w-full rounded-xl'
+                    source={{ uri: card.photoURL }} />
+                  <View className='flex-row justify-between items-center bg-white  w-full h-20 absolute bottom-0 px-6 py-2 rounded-b-xl' style={styles.cardShadow}>
+                    <View>
+                      <Text className='text-xl font-bold'>{card.displayName}</Text>
+                      <Text>{card.job}</Text>
+                    </View>
+                    <Text className='text-2xl font-bold'>{card.age}</Text>
+                  </View>
                 </View>
-                <Text className='text-2xl font-bold'>{card.age}</Text>
-              </View>
-            </View>
-          ) : (
-            <View className='relative bg-white h-3/4 rounded-xl justify-center items-center'
-              style={styles.cardShadow}>
-              <Text className='font-bold pb-5'>
-                No more profiles available
-              </Text>
-              <Image
-                className='h-20 w-full'
-                resizeMode='contain'
-                // height={100}
-                source={{ uri: "https://links.papareact.com/6gb" }}
-              />
-            </View>
-          )
-          }
-        />
+              )
+            }
+            }
+          />
+        ) : (
+          <View className='relative bg-white h-3/4 rounded-xl justify-center items-center mx-6  mt-10'
+            style={styles.cardShadow}>
+            <Text className='font-bold pb-5 text-xl'>
+              No more profiles available
+            </Text>
+            <Image
+              className='h-20 w-full'
+              resizeMode='contain'
+              // height={100}
+              source={{ uri: "https://links.papareact.com/6gb" }}
+            />
+          </View>
+        )}
       </View>
 
       {/* End of cards */}
